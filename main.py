13645ee35a5664de58e1445578811c75e44a5956
KoalaV2 <theo.johanson@gmail.com>
2021-06-24 23:31:10 +0200

    Migrated to another DB

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import time
 import threading
 import json
-import sqlite3
+import mariadb
+import os
+from dotenv import load_dotenv
 from ourgroceries import OurGroceries
 import asyncio
-import os
 from flask import Flask
 from flask import Markup
 from flask import render_template
@@ -14,31 +15,44 @@ from flask import render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-conn = sqlite3.connect('database.db', check_same_thread=False)
-c = conn.cursor()
+load_dotenv()
+DB_USER = os.getenv("DB_USER")
+DB_PASSWD = os.getenv("DB_PASSWD")
+DB_IP = os.getenv("DB_IP")
+DB_DATABASE = os.getenv("DB_DATABASE")
 
 
-def createdatabase():
-    c.execute('''CREATE TABLE IF NOT EXISTS varor (namn,typ,datum)''')
-    conn.commit()
-    conn.close()
+try:
+    conn = mariadb.connect(
+        user=DB_USER,
+        password=DB_PASSWD,
+        host=DB_IP,
+        port=3306,
+        database=DB_DATABASE
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    exit()
 
 
 def writeproduct():
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS fridge (namn VARCHAR(255),typ VARCHAR(255),datum VARCHAR(255))''')
     namn = input("Vad heter produkten? \n")
     typ = input("Vad för typ är produkten? \n")
     datum = str(input("När går denna produkt ut? \n"))
-    c.execute("INSERT INTO varor VALUES (?,?,?);", (namn, typ, datum))
+    c.execute("INSERT INTO fridge VALUES (?,?,?);", (namn, typ, datum))
     conn.commit()
     conn.close()
 
 def readdatabase():
-    cursor = c.execute("SELECT namn,typ,datum FROM varor")
-    results = cursor.fetchall()
-    return results
-    
+    c = conn.cursor()
+    cursor = c.execute("SELECT namn,typ,datum FROM fridge")
+    return c
+
 def getlist():
-    user = os.getenv('GROC_USER') 
+    user = os.getenv('GROC_USER')
     password = os.getenv('GROC_PASSWORD')
 
     og = OurGroceries(user,password)
@@ -59,11 +73,12 @@ def hello():
 def vadikylen():
     row = readdatabase()
     return render_template('readdatabase.html',data=row)
-     
+
 def main():
-    app.run(host= "192.168.68.111")
+    app.run(host= "0.0.0.0")
+    # writeproduct()
     #readdatabase()
-    #writeproduct()
+
     #getlist()
 
 if __name__ == "__main__":
